326c82ca995ba7ec7eb2fbd68d80b639dfac8485
Maxim Yefremov <maxYefr@gmail.com>
2019-08-31 18:41:11 +0300

    truffle exec hello world with instance

--- 4_hello_world.js
+++ 4_hello_world.js
@@ -1,11 +1,5 @@
 HelloWorld = artifacts.require('HelloWorld');
 module.exports = function () {
-    // const instance = HelloWorld.at(HelloWorld.address);
-    // instance.greet().then(console.log);
-    HelloWorld.deployed().then(h => console.info(h.greet().then(r => console.info("result", r)))).catch(function (e) {
-        console.error(e);
-    });
-    // HelloWorld.deployed().then(function (instance) {
-    //     console.info("instance.greet()", instance.greet());
-    // });
+    const instance = HelloWorld.at(HelloWorld.address);
+    instance.then(i => i.greet().then(console.log));
 }
