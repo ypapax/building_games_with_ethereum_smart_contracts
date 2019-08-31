294de63f53d31cbbe4f6d204e552ad2016f14da5
Maxim Yefremov <maxYefr@gmail.com>
2019-08-31 18:29:32 +0300

    prints result

--- 4_hello_world.js
+++ 4_hello_world.js
@@ -2,7 +2,9 @@ HelloWorld = artifacts.require('HelloWorld');
 module.exports = function () {
     // const instance = HelloWorld.at(HelloWorld.address);
     // instance.greet().then(console.log);
-    HelloWorld.deployed().then(h => h.greet());
+    HelloWorld.deployed().then(h => console.info(h.greet().then(r => console.info("result", r)))).catch(function (e) {
+        console.error(e);
+    });
     // HelloWorld.deployed().then(function (instance) {
     //     console.info("instance.greet()", instance.greet());
     // });
