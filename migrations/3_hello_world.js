0906af4911f170aa244101dcc6e729951a4d76d0
Maxim Yefremov <maxYefr@gmail.com>
2019-08-31 17:04:21 +0300

    reading pass from env var during truffle migration to rinkeby

--- migrations/3_hello_world.js
+++ migrations/3_hello_world.js
@@ -1,6 +1,13 @@
 var HelloWorld = artifacts.require("./HelloWorld.sol");
 // and then you can call it in develop console by:
 // HelloWorld.deployed().then(h => h.greet());
-module.exports = function (deployer) {
+module.exports = function (deployer, network) {
+    // unlock account for geth
+    if (network === "rinkeby" || network === "mainnet") {
+        const accountNumber = 2;
+        const password = process.env.RINKBEBY_ACCOUNT5_PASSWORD;
+        web3.personal.unlockAccount(web3.eth.accounts[accountNumber], password)
+    }
+    console.info("network", network);
     deployer.deploy(HelloWorld);
 };
