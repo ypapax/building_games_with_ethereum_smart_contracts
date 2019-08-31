df0b89840c13f8dc1323eca0e0bf60165b55cc43
Maxim Yefremov <maxYefr@gmail.com>
2019-08-31 16:32:49 +0300

    added comment

--- migrations/3_hello_world.js
+++ migrations/3_hello_world.js
@@ -1,5 +1,6 @@
 var HelloWorld = artifacts.require("./HelloWorld.sol");
+// and then you can call it in develop console by:
+// HelloWorld.deployed().then(h => h.greet());
 module.exports = function (deployer) {
     deployer.deploy(HelloWorld);
-    HelloWorld.deployed().then(h => h.greet());
 };
